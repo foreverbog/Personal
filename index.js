1f73f7cf524a554aa7fccf64015c9e6a33e4f597
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-03-18 20:05:33 +0100

    home page styled and functions added

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-document.querySelector('#email').style.color = 'red'
-document.querySelector('#email').addEventListener('click', () => {
+const logInBtn = document.querySelector('.logInBtn')
+const logInForm = document.querySelector('.logInForm')
+const guestcontainer = document.querySelector('.guestcontainer')
+const closeBtn = document.querySelector('.closeBtn')
+const closeBtn2 = document.querySelector('.closeBtn2')
+const registerBtn = document.querySelector('.registerBtn')
+const registerForm = document.querySelector('.registerForm')
+
+logInBtn.addEventListener('click', () => {
+    guestcontainer.classList.add('hidden')
+    logInForm.classList.remove('hidden')
+})
+registerBtn.addEventListener('click', () => {
+    guestcontainer.classList.add('hidden')
+    registerForm.classList.remove('hidden')
+})
+
+closeBtn.addEventListener('click', () => {
+    guestcontainer.classList.remove('hidden')
+    logInForm.classList.add('hidden')
+})
+closeBtn2.addEventListener('click', () => {
+    guestcontainer.classList.remove('hidden')
+    registerForm.classList.add('hidden')
     
 })
