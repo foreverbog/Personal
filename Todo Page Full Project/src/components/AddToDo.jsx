e08bb4a34e467bec5ac9ab9a628bb35d1ad15c09
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-04-12 15:30:35 +0200

    add todos logic, works properly now

--- Todo Page Full Project/src/components/AddToDo.jsx
+++ Todo Page Full Project/src/components/AddToDo.jsx
@@ -1,5 +1,23 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 const AddToDo = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+  //Get input Value
+  const handleChange = (e) => {
+    const newTask = e.target.value;
+    setTask(newTask);
+  };
+  //Store the task
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task === "") {
+      alert("Please enter a task");
+    } else {
+      setTodos([...todos, task]);
+      setTask("");
+    }
+  };
   return (
     <motion.div
       className="form main__container"
@@ -8,28 +26,35 @@ const AddToDo = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
     >
-      <form className="flex flex-col">
+      <form onSubmit={handleSubmit} className="flex flex-col">
         <label htmlFor="addTask">Add Task</label>
-        <div className="flex bg-red-500 rounded-xl ">
+        <div className="flex  rounded-xl ">
           <input
-            className="border-none flex-1 bg-transparent p-4 outline-none"
+            className="loginInput border-none flex-1 bg-transparent p-4 outline-none"
             type="text"
             id="addTask"
             name="addTask"
             placeholder="Add your task"
+            value={task}
+            onChange={handleChange}
           />
           <button
-            className="border-none outline-none bg-blue-200 rounded-xl p-8 m-0"
+            className="border-none outline-none bg-blue-200 rounded-xl p-4 m-0"
             type="submit"
           >
             ADD
           </button>
         </div>
       </form>
-      <div className="flex">
-        <pc className="flex-1">Task 1</pc>
-        <button>Edit</button>
-        <button>Delete</button>
+      <div className="flex flex-col">
+        {todos.map((todo) => (
+          <div key={crypto.randomUUID()} className="flex">
+            <p className="flex-1">{todo}</p>
+
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        ))}
       </div>
     </motion.div>
   );
