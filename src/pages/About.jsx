6674cb3863eaae25077d42ba86bce56cdd9d37e4
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-05-01 01:41:50 +0200

    fixed margins fail for smallers screens

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   const [isUserEvents, setIsUserEvents] = useState(false);
   return (
     <motion.div
-      className="main__container bg-primary-light dark:bg-primary-dark dark:text-white/60 p-4   font-title overflow-auto max-[1023px]:max-h-[500px] m-20 rounded-xl shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
+      className="main__container bg-primary-light dark:bg-primary-dark dark:text-white/60 p-4   font-title overflow-auto max-[1023px]:max-h-[500px] m-4 lg:m-20 rounded-xl shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
